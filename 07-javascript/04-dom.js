20c88c42193967e0a0bebd81a0048c873fd37cef
Torin Costales <tcostal2@pdx.edu>
2025-11-07 10:38:35 -0800

    Exercises 04 JS

--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -15,3 +15,25 @@
 // add an event listener on the target element
 
 // callback function to handle event
+const input = document.querySelector("#userInput");
+const copyButton = document.querySelector("#copy");
+const output = document.querySelector("#output");
+
+copyButton.addEventListner("click", handleClick);
+
+function copyText() {
+  outputDiv.textContent = input.value;
+}
+
+let userInput2 = document.querySelector("#userInput2");
+userInput2.addEventListener("input", handleInput);
+
+let section = document.querySelector("#inputEventExample");
+let element = document.createElement("div");
+element.setAttribute("class", "outpout");
+section.append(element);
+
+function handleInput(event) {
+  console.log(userInput2.value);
+  Element.textContent = userInput2.value;
+}
